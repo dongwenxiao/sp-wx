c9d41ce1a4dea6c0616908e1eb709b9cc49e0b40
dongwenxiao <cs_victor@126.com>
2017-11-20 14:16:45 +0800

    add enum

---
+++ enum.js
@@ -0,0 +1,12 @@
+module.exports = {
+    WX_OAUTH_SCOPE: {
+        SNSAPI_USERINFO: 'snsapi_userinfo',
+        SNSAPI_BASE: 'snsapi_base'
+    },
+    GET_OAuth_URL: 'https://open.weixin.qq.com/connect/oauth2/authorize',
+    GET_OAuth_ACCESS_TOKEN_URL: 'https://api.weixin.qq.com/sns/oauth2/access_token',
+    GET_ACCESS_TOKEN_URL: 'https://api.weixin.qq.com/cgi-bin/token',
+    GET_USER_INFO_URL: 'https://api.weixin.qq.com/sns/userinfo',
+    GET_TICKET_URL: 'https://api.weixin.qq.com/cgi-bin/ticket/getticket',
+    GET_MEDIA_FILE_URL: 'http://file.api.weixin.qq.com/cgi-bin/media/get'
+}
